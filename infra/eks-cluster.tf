f7d9893a58c5c7b26b73067312643bb4a79e5aec
jayeshbanait <109805702+jayeshbanait@users.noreply.github.com>
2022-07-26 12:02:40 +0530

    Update eks-cluster.tf

--- infra/eks-cluster.tf
+++ infra/eks-cluster.tf
@@ -1,3 +1,10 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
